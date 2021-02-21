1b088e22194d0e7d2649edbc1d427b7d0919d6dd
Cristian Sáez <cristiansaez97@gmail.com>
2021-02-21 15:05:22 -0300

    Se agrega efecto de apertura menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,28 @@ if(document.querySelector('#ingreso')) {
 }
 if(document.querySelector('.menu-btn')) {
     document.querySelector('.menu-btn').addEventListener('click', e => {
-        document.querySelector('.navegacion').estAttribute('display', 'block');
+        if(document.querySelector('.navegacion').style.display == 'block') {
+            document.querySelector('.navegacion').style.display = 'none';
+            document.querySelector('header').style.height = '50px';
+            document.querySelector('#x1').style.transform = '';
+            document.querySelector('#x2').style.transform = '';
+            document.querySelector('#x3').style.display = 'block';
+        } else {
+            let pos = 50;
+            let t =  setInterval(move, 10);
+
+            function move() {
+                if(pos >= 200) {
+                    document.querySelector('.navegacion').style.display = 'block';
+                    document.querySelector('#x1').style.transform = 'rotate(45deg) translateY(5px) translateX(5px)';
+                    document.querySelector('#x2').style.transform = 'rotate(-45deg)';
+                    document.querySelector('#x3').style.display = 'none';
+                    clearInterval(t);
+                } else {
+                    pos += 1;
+                    document.querySelector('header').style.height = `${pos}px`;
+                }
+            }
+        }
     });
 }
