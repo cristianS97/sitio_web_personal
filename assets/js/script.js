27a69202f8c3ffb272b26dfba39ccebdeabdf37c
Cristian Sáez <cristiansaez97@gmail.com>
2021-02-20 22:56:44 -0300

    Avances en el header, se empieza el menu responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
+// Efecto de ingreso del sitio
 if(document.querySelector('#ingreso')) {
     document.querySelector('#ingreso').addEventListener('click', e => {
         e.preventDefault();
-        document.querySelector('#ingreso').classList.add('dn');
     
-        document.querySelector('.inicio').classList.add('cerrar');
         document.querySelector('.principal').classList.remove('dn');
-    
-        document.querySelector('.inicio').classList.remove('alto');
-        document.querySelector('.inicio').classList.add('dn');
+        document.querySelector('.inicio').setAttribute('class', 'cerrar');
+        document.querySelector('#ingreso').classList.add('dn');
+    });
+}
+if(document.querySelector('.menu-btn')) {
+    document.querySelector('.menu-btn').addEventListener('click', e => {
+        document.querySelector('.navegacion').estAttribute('display', 'block');
     });
 }
